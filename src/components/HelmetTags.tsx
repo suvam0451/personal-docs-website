c446f35473fd1a7f8a183a0c4a96e33eaa967376
Debashish Patra <suvam0451@outlook.com>
2020-08-06 23:45:42 +0530

    Helmet tags

---
+++ src/components/HelmetTags.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+type IBlogpostSEOProps = {
+	title: string;
+};
+
+export function BlogpostSEO(props: IBlogpostSEOProps) {
+	return (
+		<Helmet>
+			<title>{props.title}</title>
+			<meta name="description" content={data.data.mdx.frontmatter.description} />
+			<meta
+				name="og:title"
+				property="og:title"
+				content={data.data.mdx.frontmatter.seotitle}
+			/>
+			<meta name="twitter:card" content={data.data.mdx.frontmatter.description} />
+			<link rel="canonical" href={data.data.mdx.frontmatter.path} />
+			<meta name="viewport" content="width=device-width, initial-scale=1" />
+		</Helmet>
+	);
+}
